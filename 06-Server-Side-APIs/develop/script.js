3fbb31c48cb39cc2eb60ff422e9af01713f27576
Kyle Kleven <kylekleven8@gmail.com>
2021-01-27 01:19:38 -0600

    Adjusted HTML layout to accomodate main weather card, and populated card data via api (except for UV index)

--- 06-Server-Side-APIs/develop/script.js
+++ 06-Server-Side-APIs/develop/script.js
@@ -2,30 +2,32 @@
 
 var APIkey = "1c0d3dc2df97a78b630d4385a864717d";
 var cities = [];
-var date = moment();
+var date = moment().format("l");
 
-$('#searchEl').on("click", function(event){
+$('#submit').on("click", function(event){
     event.preventDefault();
 
-    var city = $('#cityEl').val().trim();
-    
+    var city = $('#searchInput').val().trim();
+
     cities.push(city);
+    console.log(cities);
    
-    showWeather();
+    showWeather(city);
+    renderHistory();
+    //saveSearch(cities);
 })
 
 function renderHistory () {
     $('.historyDiv').empty();
+    //$('.historyDiv').text("HISTORY LIST");
 
     
 }
 
-function showWeather() {
+function showWeather(city) {    
 
-    city = $(this).data('city');
-
-    var weatherURL = "api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIkey;
-    var forecastURL = "api.openweathermap.org/data/2.5/forecast?q=" + input + "&appid=" + APIkey;
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
+    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIkey;
 
     $.ajax({
         url: weatherURL,
@@ -35,17 +37,42 @@ function showWeather() {
 
         console.log(weatherResponse);
 
+        var latitude = weatherResponse.coord.lat;
+        var longitude = weatherResponse.coord.lon;
+        var uviURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIkey;
+    
+        $.ajax({
+            url: uviURL,
+            method: "GET",
+            async: false
+        })
+        .then(function(uviResponse) {
+            console.log(uviResponse.value); 
+        })
+            
+        var cardTitleEl = $('#mainCardTitle').html(city + " <br> " + date);
+        var tempEl = $('#temperature').html("Temperature: " + Math.floor(weatherResponse.main.temp) + "&deg");
+        var humidEl = $('#humidity').html("Humidity: " + Math.floor(weatherResponse.main.humidity) + "&deg");
+        var windEl = $('#wind').html("Wind Speed: " + weatherResponse.wind.speed + " mph");
+        var uviEl = $('#uvi').html("UV Index: ");
 
+        $('#mainCardTitle').prepend(cardTitleEl);
+        $('#mainCardDetails').append(tempEl, humidEl, windEl, uviEl);
+
+        $.ajax({
+            url:forecastURL,
+            method: "GET",
+            async: false
+        })
+        .then(function(forecastResponse) {
+            console.log(forecastResponse);
+            var forecast = moment(forecastResponse.list[1].dt).format('l')
+            for (var i = 0; i < 6; i++) {
+                console.log(forecast);
+            }
+        })
     })
 
-    $.ajax({
-        url:forecastURL,
-        method: "GET"
-    })
-    .then(function(forecastResponse) {
-        console.log(forecastResponse);
-
-    })
+    
 
 }
-
