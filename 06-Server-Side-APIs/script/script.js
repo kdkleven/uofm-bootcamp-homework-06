585dace3d3690a052be8afdbce121ed9932c9526
SageTengu <kyle.kleven@outlook.com>
2021-01-28 18:32:21 -0600

    Updated styling, fixed UV index and icons display

--- 06-Server-Side-APIs/script/script.js
+++ 06-Server-Side-APIs/script/script.js
@@ -1,39 +1,30 @@
 
 
 var APIkey = "1c0d3dc2df97a78b630d4385a864717d";
-var cities = [];
+var searchHistory = [];
 var date = moment().format("l");
 
 $('#submit').on("click", function (event) {
     event.preventDefault();
+    $('.card-text').html = "";
+    $('.card-body').html = "";
+    $('<img>').html = "";
+    $('<h6>').html = "";
+    $('.currentCard').html = "";
 
-    var city = $('#searchInput').val().trim();
+    var search = $('#searchInput').val().trim();
+    
+    searchHistory.push(search);
+    console.log(searchHistory);
 
-    cities.push(city);
-    console.log(cities);
-
-    findWeather(city);
+    findWeather(search);
     //showWeather();
-    renderHistory();
-    saveSearch(cities);
+    renderHistory(searchHistory);
+    saveSearch(searchHistory);
 });
 
-function renderHistory() {
-    $('.historyDiv').empty();
-    //$('.historyDiv').text("HISTORY LIST");
-
-
-}
-
-function saveSearch(cities) {
-    localStorage.setItem("history", cities);
-}
-
-function findWeather(city) {
-
-    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + APIkey;
-    var forecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&cnt=5&appid=" + APIkey;
-    
+function findWeather(search) {
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&units=imperial&appid=" + APIkey;
 
     $.ajax({
         url: weatherURL,
@@ -41,53 +32,84 @@ function findWeather(city) {
     })
         .then(function (weatherResponse) {
 
-            console.log(weatherResponse);
+           // console.log(weatherResponse);
 
             var latitude = weatherResponse.coord.lat;
             var longitude = weatherResponse.coord.lon;
-            var uviURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIkey;
-            var uvIndex = "";
+            
+            var icon = weatherResponse.weather[0].icon;
+            var iconURL = "http://openweathermap.org/img/wn/" + icon + ".png";
+            
+            $('.mainCardDetails').prepend($('<h3>').html("Today's Weather"));
 
-            calculateUV(uviURL, uvIndex);
-            getForecast(latitude, longitude);
-
+            var cardTitleEl = $('#mainCardTitle').html(search + " " + "(" + date + ")");
+            
+            
             // Declare variables to display on the main card
-            var cardTitleEl = $('#mainCardTitle').html(city + " " + "(" + date + ")");
-            cardTitleEl.css("font-size: 20px");
-            var sunEl = $('<img>').attr("src","");
+            
+            //var todayTitle = $('<h3>').attr({id: "today", value: "Today's Weather"});
+            
+            var iconEl = $('<img>').attr({ id: "mWeather", src: iconURL });
             var tempEl = $('#temperature').html("Temperature: " + Math.floor(weatherResponse.main.temp) + "&deg");
             var humidEl = $('#humidity').html("Humidity: " + Math.floor(weatherResponse.main.humidity) + "&deg");
             var windEl = $('#wind').html("Wind Speed: " + weatherResponse.wind.speed + " mph");
-            var uviEl = $('#uvi').html("UV Index: " + uvIndex);
 
-            // Display values on main card
-            // Ensure UV index is color-coded as favorable, moderate, severe
-            // display current day's weather (clouds, sun, rain, snow, etc.)
-            $('#mainCardTitle').prepend(cardTitleEl);
-            $('#mainCardDetails').append(sunEl, tempEl, humidEl, windEl, uviEl);
-
-            // getForecast(forecastURL);
+            saveSearch(cardTitleEl, iconEl, tempEl, humidEl, windEl);
+            displayWeather(cardTitleEl, iconEl, tempEl, humidEl, windEl);
+            calculateUV(latitude, longitude);
+            getForecast(latitude, longitude);
 
         });
 }
 
+function displayWeather(cardTitleEl, iconEl, tempEl, humidEl, windEl) {
+    // Display values on main card
+    // Ensure UV index is color-coded as favorable, moderate, severe
+    // display current day's weather (clouds, sun, rain, snow, etc.)
+    $('#mainCardTitle').append(cardTitleEl);
+    $('.mainCardDetails').append(iconEl, tempEl, humidEl, windEl);
+
+}
+
+function saveSearch(searchHistory) {
+    
+    localStorage.setItem("searches", searchHistory);
+    renderHistory(searchHistory);
+
+}
+
+
+function renderHistory(searchHistory) {
+    $('.historyDiv').empty();
+    $('.historyDiv').text("HISTORY LIST");
+    
+
+
+    // for (var i = 0; i < searchHistory.length; i++) {
+    //     localStorage.getItem("searches");   
+    //     //$('<button>').attr({id: })
+    // }
+
+}
+
 // Calculate and return UV index
-function calculateUV(uviURL, uvIndex) {
+function calculateUV(latitude, longitude) {
+
+    var uviURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIkey;
+
     $.ajax({
         url: uviURL,
         method: "GET",
-        async: false,
-    })
-        .then(function (uviResponse) {
-            console.log("before: " + uviResponse.value);
-            uvIndex = uviResponse.value;
-            return (uvIndex);
-        });
+        success: function (data) {
+            var uviEl = $('#uvi').html("UV Index: " + data.value);
+            $('.mainCardDetails').append(uviEl);
+        }
+    });
 }
 
 function getForecast(latitude, longitude) {
 
-    var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current+minutely+hourly+alerts&units=imperial&appid=" + APIkey;    
+    var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current+minutely+hourly+alerts&units=imperial&appid=" + APIkey;
 
     // Call for 5-day forecast
     $.ajax({
@@ -95,40 +117,45 @@ function getForecast(latitude, longitude) {
         method: "GET",
     })
         .then(function (forecastResponse) {
-            console.log(forecastResponse);
+           // console.log(forecastResponse);
 
             var forecast = [
                 {
-                    date: "", weather: "", temp: "", humidity: ""
+                    date: "", icon: "", temp: "", humidity: ""
                 },
                 {
-                    date: "", weather: "", temp: "", humidity: ""
+                    date: "", icon: "", temp: "", humidity: ""
                 },
                 {
-                    date: "", weather: "", temp: "", humidity: ""
+                    date: "", icon: "", temp: "", humidity: ""
                 },
                 {
-                    date: "", weather: "", temp: "", humidity: ""
+                    date: "", icon: "", temp: "", humidity: ""
                 },
                 {
-                    date: "", weather: "", temp: "", humidity: ""
+                    date: "", icon: "", temp: "", humidity: ""
                 },
-            ]
+            ];
 
             for (var i = 0; i < forecast.length; i++) {
                 forecast[i].date = moment.unix(forecastResponse.daily[i].dt).format('l');
-                forecast[i].weather = forecastResponse.daily[i].weather[0].main;
+                forecast[i].icon = forecastResponse.daily[i].weather[0].icon;
                 forecast[i].temp = forecastResponse.daily[i].temp.day;
                 forecast[i].humidity = forecastResponse.daily[i].humidity;
 
-                $('#fcDate-' + [i]).append(forecast[i].date);
-                $('#fcWeather-' + [i]).html("src", "placeholder");
+                var iconURL = "http://openweathermap.org/img/wn/" + forecast[i].icon + ".png";
+                var imgEl = $('<img>').attr({ id: "#fcWeather-" + [i], src: iconURL });
+
+                $('#fcDate-' + [i]).append(forecast[i].date, imgEl);
+
+                //console.log(iconURL);
+
                 $('#fcTemp-' + [i]).html("Temp: " + Math.floor(forecast[i].temp) + "&deg");
                 $('#fcHumid-' + [i]).html("Humid: " + Math.floor(forecast[i].humidity) + "&deg");
-                
+
             }
 
-            console.log(forecast);
-       
+            //console.log(forecast);
+
         });
 }
