9432e6e1205fb41a3d9fa21ac2bd66a48a1dbc6a
SageTengu <kyle.kleven@outlook.com>
2021-01-29 01:44:57 -0600

    Display buttons of past searches when a new search is performed.

--- 06-Server-Side-APIs/script/script.js
+++ 06-Server-Side-APIs/script/script.js
@@ -5,120 +5,128 @@ var date = moment().format("l");
 
 $('#submit').on("click", function (event) {
     event.preventDefault();
+
+    var search = $('#searchInput').val().trim();
+
+    if (search === "" || search === null) {
+        return;
+    }
+  
     $('.todayDiv').removeClass('hidden');
     $('.forecastDiv').removeClass('hidden');
 
-    var search = $('#searchInput').val().trim();
-    
-    saveSearch(search);
     searchWeather(search);
+    saveSearch(search);
+
 });
 
-$('.historyBtn').on('click', function(event) {
+
+$('#historyBtn-0').click(function(event){
     event.preventDefault();
-    searchWeather(this);
+    console.log("clicked");
     
+    var pastSearch = $(this).val();
+    
+    searchWeather(pastSearch);
+
 });
 
+// call openweathermap APIs to get weather
 function searchWeather(search) {
     var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + search + "&units=imperial&appid=" + APIkey;
 
+    // call weather API to get today's weather
     $.ajax({
         url: weatherURL,
         method: "GET"
     })
         .then(function (weatherResponse) {
 
-           // console.log(weatherResponse);
+            // console.log(weatherResponse);
 
             var latitude = weatherResponse.coord.lat;
             var longitude = weatherResponse.coord.lon;
-            
+            var uviURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIkey;
+            var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current+minutely+hourly+alerts&units=imperial&appid=" + APIkey;
             var icon = weatherResponse.weather[0].icon;
             var iconURL = "http://openweathermap.org/img/wn/" + icon + ".png";
-            
-            $('#todayTitle').html("Current Weather");
 
             // Declare variables to display on the main card            
+            $('#todayTitle').html("Current Weather");
             $('#mainCardTitle').html(search + " " + "(" + date + ")");
             $('#mainIcon').attr("src", iconURL);
             $('#temperature').html("Temperature: " + Math.floor(weatherResponse.main.temp) + "&deg");
             $('#humidity').html("Humidity: " + Math.floor(weatherResponse.main.humidity) + "&deg");
             $('#wind').html("Wind Speed: " + weatherResponse.wind.speed + " mph");
 
-            calculateUV(latitude, longitude);
-            getForecast(latitude, longitude);
+
+            // Call uvi API to get UV index    
+            $.ajax({
+                url: uviURL,
+                method: "GET",
+                success: function (data) {
+                    var uviEl = $('#uvi').html("UV Index: " + data.value);
+                    $('.mainCardDetails').append(uviEl);
+                }
+            });
+
+            // Call onecall API to get 5-day forecast
+            $.ajax({
+                url: forecastURL,
+                method: "GET",
+            })
+                .then(function (forecastResponse) {
+
+                    for (var i = 0; i < 5; i++) {
+                        fDate = moment.unix(forecastResponse.daily[i].dt).format('l');
+                        fIcon = forecastResponse.daily[i].weather[0].icon;
+                        fTemp = forecastResponse.daily[i].temp.day;
+                        fHumid = forecastResponse.daily[i].humidity;
+
+                        var iconURL = "http://openweathermap.org/img/wn/" + fIcon + ".png";
+
+                        $('#dayicon-' + i).attr("src", iconURL);
+                        $('#fcDate-' + i).html(fDate);
+                        $('#fcTemp-' + [i]).html("Temp: " + Math.floor(fTemp) + "&deg");
+                        $('#fcHumid-' + [i]).html("Humid: " + Math.floor(fHumid) + "&deg");
+
+                    }
+                });
 
         });
+    }
+
+var savedSearch = [];
+
+// save the current search parameter to local storage and display a corresponding button
+function saveSearch(searchJSON) {
+    
+    savedSearch.push(searchJSON);
+    
+    localStorage.setItem("history", JSON.stringify(savedSearch));
+    console.log(savedSearch);
+    renderHistory();
 }
 
-// Calculate and return UV index
-function calculateUV(latitude, longitude) {
-
-    var uviURL = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + APIkey;
-
-    $.ajax({
-        url: uviURL,
-        method: "GET",
-        success: function (data) {
-            var uviEl = $('#uvi').html("UV Index: " + data.value);
-            $('.mainCardDetails').append(uviEl);
-        }
-    });
-}
-
-// Get 5-day forecast and render it to the page
-function getForecast(latitude, longitude) {
-
-    var forecastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&exclude=current+minutely+hourly+alerts&units=imperial&appid=" + APIkey;
-
-    // Call for 5-day forecast
-    $.ajax({
-        url: forecastURL,
-        method: "GET",
-    })
-        .then(function (forecastResponse) {
-           //console.log(forecastResponse);
-
-            for (var i = 0; i < 5; i++) {
-                fDate = moment.unix(forecastResponse.daily[i].dt).format('l');
-                fIcon = forecastResponse.daily[i].weather[0].icon;
-                fTemp = forecastResponse.daily[i].temp.day;
-                fHumid = forecastResponse.daily[i].humidity;
-
-                var iconURL = "http://openweathermap.org/img/wn/" + fIcon + ".png";                
-                
-                $('#dayicon-'+ i).attr("src", iconURL);
-                $('#fcDate-' + i).html(fDate);
-                $('#fcTemp-' + [i]).html("Temp: " + Math.floor(fTemp) + "&deg");
-                $('#fcHumid-' + [i]).html("Humid: " + Math.floor(fHumid) + "&deg");
-
-            }
-        });
-}
-
-function saveSearch(search) {
-    
-    localStorage.setItem("searches", search);
-    
-    var searchHistory = $('<button>').html(search);
-    
-    searchHistory.attr({class: 'historyBtn btn btn-light', type: 'button'});
-    searchHistory.css('margin', '5px');
-    
-    $('.historyDiv').append(searchHistory);
-
-    $('#searchInput').val('');
-
-    renderHistory(searchHistory);
-
-}
-
+// render past searches as buttons on the page
 function renderHistory() {
+    $('#searchInput').val('');
     
-    localStorage.getItem("searches");
+    var lastSearch = JSON.parse(localStorage.getItem("history"));
+    var renderBtn = $('<button>').attr('type', 'button');
+    renderBtn.attr('class', 'btn btn-light historyBtn');
+    renderBtn.css("margin", "5px");
 
-
+    for (var i = 0; i < savedSearch.length; i++) {
+    
+        if (lastSearch !== null) {
+    
+            renderBtn.attr('id', 'historyBtn-' + i);
+            renderBtn.html(lastSearch[i]);  
+            $('.historyDiv').append(renderBtn);
+        }   
+    }
 }
 
-
+// display buttons for past searches (if they exist)
+renderHistory();
